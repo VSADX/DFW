7b75c0c310c1fc7271c6b5d8f7bdc2e22776c1f0
VSADX <81491645+VSADX@users.noreply.github.com>
2021-10-20 13:50:00 -0500

    Update destiny-vendor-jsx.d.ts

--- destiny-vendor-jsx.d.ts
+++ destiny-vendor-jsx.d.ts
@@ -1,9 +1,9 @@
-/// <reference path="./destiny.d.ts" />
-/// <reference path="./x-destiny.d.ts" />
+/// <reference path="./destiny-ui.d.ts" />
+/// <reference path="./destiny-jsx.d.ts" />
 
 export {
     CSSTemplate, Component, ReactiveArray, ReactiveValue, ReadonlyReactiveArray, ReadonlyReactiveValue, Ref, TReactive, TReactiveEntity, TReactiveProperties, TReactiveValueType, TemplateResult, attachCSSProperties, classNames, computed, getElementData, reactive, reactiveProperties, register 
-} from "destiny-ui"
+} from "@destiny-ui"
 export {
     make, makeFrom, xml, css, html
 } from "destiny-ui-x"
